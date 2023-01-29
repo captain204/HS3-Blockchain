02309fe1e858cd20816debdb56ab38df479cbf11
NURU <nurudeenakindele8@gmail.com>
2023-01-29 16:09:57 +0100

    Public Subscription System

---
+++ backend/pubsub.py
@@ -0,0 +1,74 @@
+import time
+
+from pubnub.pubnub import PubNub
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.callbacks import SubscribeCallback
+
+from backend.blockchain.block import Block
+
+pnconfig = PNConfiguration()
+
+pnconfig.subscribe_key = 'sub-c-94929cb1-247a-4337-a58d-be5eb576a84e'
+pnconfig.publish_key ='pub-c-0083bed6-c7f6-4052-bb52-920a7db44710'
+
+
+
+
+CHANNELS ={
+    'TEST':'TEST',
+    'BLOCK':'BLOCK'
+}
+
+
+class Listener(SubscribeCallback):
+    def __init__(self, blockchain):
+        self.blockchain = blockchain
+        
+
+    def message(self, pubnub, message_object):
+        print(f'\n Incoming message_object{message_object.channel} |Message{message_object.message}')
+
+        if message_object.channel == CHANNELS['BLOCK']:
+            block  = Block.from_json(message_object.message)
+            potential_chain = self.blockchain.chain[:]
+            potential_chain.append(block)
+
+            try:
+                self.blockchain.replace_chain(potential_chain)
+                print('\n Successfully replaced the local chain')
+            except Exception as e:
+                print(f'\n --Did not replace chain {e}')
+
+
+
+class Pubsub():
+    """
+        Handles the public susbscribe layer of the application
+        Provides communication between the nodes of the blockchain network
+    """
+    def __init__(self,blockchain):
+        self.pubnub = PubNub(pnconfig)
+        self.pubnub.subscribe().channels([CHANNELS.values()]).execute()
+        self.pubnub.add_listener(Listener(blockchain))
+
+    def publish(self, channel, message):
+        """
+            Publish the message object to the channel
+        """
+        self.pubnub.publish().channel(channel).message(message).sync()
+
+    def broadcast_block(self,block):
+        """
+            Broadcast a block object to all nodes
+        """
+        self.publish(CHANNELS['BLOCK'], block.to_json())
+
+def main():
+    pubsub = Pubsub()
+    time.sleep(1)
+    pubsub.publish(CHANNELS['TEST'], {'foo':'bar'})
+    
+
+if __name__  == '__main__':
+    main()
+
