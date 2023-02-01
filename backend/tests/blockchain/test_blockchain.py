b52ac2e460a04cc72028b51e03146954f42f2808
NURU <nurudeenakindele8@gmail.com>
2023-02-01 12:17:53 +0100

    Modified Blockchain Fixture Test

--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -20,39 +20,39 @@ def test_add_block():
 
 
 @pytest.fixture
-def create_three_blocks():
+def blockchain_three_blocks():
     blockchain = Blockchain()
     for i in range(3):
         blockchain.add_block(i)
     return blockchain
 
-def test_is_valid_chain(create_three_blocks):
-    Blockchain.is_valid_chain(create_three_blocks.chain)
+def test_is_valid_chain(blockchain_three_blocks):
+    Blockchain.is_valid_chain(blockchain_three_blocks.chain)
 
 
-def test_is_valid_chain_bad_genesis(create_three_blocks):
-    create_three_blocks.chain[0].hash = 'wrong hash'
+def test_is_valid_chain_bad_genesis(blockchain_three_blocks):
+    blockchain_three_blocks.chain[0].hash = 'wrong hash'
 
     with pytest.raises(Exception, match='The genesis block must be valid'):
-        Blockchain.is_valid_chain(create_three_blocks.chain)
+        Blockchain.is_valid_chain(blockchain_three_blocks.chain)
 
-def test_replace_chain(create_three_blocks):
+def test_replace_chain(blockchain_three_blocks):
     blockchain = Blockchain()
-    blockchain.replace_chain(create_three_blocks.chain)
-    assert blockchain.chain == create_three_blocks.chain
+    blockchain.replace_chain(blockchain_three_blocks.chain)
+    assert blockchain.chain == blockchain_three_blocks.chain
 
-def test_replace_chain_not_longer(create_three_blocks):
+def test_replace_chain_not_longer(blockchain_three_blocks):
     blockchain = Blockchain()
 
     with pytest.raises(Exception, match='Cannot be replaced. Incoming chain must be longer'):
-        create_three_blocks.replace_chain(blockchain.chain)
+        blockchain_three_blocks.replace_chain(blockchain.chain)
 
-def test_replace_chain_with_bad_chain(create_three_blocks):
+def test_replace_chain_with_bad_chain(blockchain_three_blocks):
     blockchain = Blockchain()
-    create_three_blocks.chain[1].hash = 'wrong hash'
+    blockchain_three_blocks.chain[1].hash = 'wrong hash'
 
     with pytest.raises(Exception, match='Cannot be replaced. The incoming chain is invalid'):
-        blockchain.replace_chain(create_three_blocks.chain)
+        blockchain.replace_chain(blockchain_three_blocks.chain)
 
 
 
